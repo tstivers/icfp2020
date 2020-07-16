551667b29eb805eeaac62d854e2fc60e31d566c7
Tom Stivers <tstivers@gmail.com>
2020-07-16 17:08:07 -0500

    Add project files.

---
+++ app/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    public static class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var serverUrl = args[0];
+            var playerKey = args[1];
+            Console.WriteLine($"ServerUrl: {serverUrl}; PlayerKey: {playerKey}");
+
+            if (!Uri.TryCreate(serverUrl, UriKind.Absolute, out var serverUri))
+            {
+                Console.WriteLine("Failed to parse ServerUrl");
+                return 1;
+            }
+
+            using var httpClient = new HttpClient { BaseAddress = serverUri };
+            var requestContent = new StringContent(playerKey, Encoding.UTF8, MediaTypeNames.Text.Plain);
+            using var response = await httpClient.PostAsync("", requestContent);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"Unexpected server response: {response}");
+                return 2;
+            }
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Server response: {responseString}");
+
+            return 0;
+        }
+    }
+}
